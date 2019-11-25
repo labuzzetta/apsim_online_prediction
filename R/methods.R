20bf782cd96e9674a2c1d8eef97736b2b70ea0ac
clabuzze <clabuzze@iastate.edu>
2019-11-25 09:24:35 -0600

    Update AR method code from Maize.Wt.Change

--- R/methods.R
+++ R/methods.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(lubridate)
 library(dplyr)
 library(mgcv)
+library(purrr)
 
 #Load datasets from package
 data("training")
@@ -19,5 +20,64 @@ tst <- testing %>% arrange(Date) %>%
   mutate(Maize.Wt.Change = Maize.AboveGround.Wt - lag(Maize.AboveGround.Wt))
 tst[1, "Maize.Wt.Change"] <- 0
 
+#AR Method
+detrend.fit <- gam(Maize.Wt.Change ~ s(Weather.Rain) + s(Weather.Radn) + 
+                     s(Weather.MaxT) + s(Weather.MeanT) + s(Weather.MinT) +
+                     s(Weather.VPD) + s(yday), data = trn)
 
+combo <- rbind(trn, tst)
+testing_index <- which(year(combo$Date) >= 2005 & year(combo$Date) < 2012)
 
+#Use model to make generalized prediction on future meteorological variables
+combo$Trend <- c(predict(detrend.fit), predict(detrend.fit,newdata = tst))
+#Detrend the predictions, essentially calculating residuals
+combo$Maize.Wt.Change.detrend <- combo$Maize.Wt.Change - combo$Trend
+
+#Model residuals via online prediction method by updating AR model
+#with newly observed data after each iteration
+ar.forecast <- numeric(length(testing_index))
+harvested = FALSE
+year.tot = 0
+for (i in seq(testing_index)) {
+  
+  #Fit AR model on previously observed residuals
+  ar.fit <- ar(combo$Maize.Wt.Change.detrend[1:(testing_index[i] - 1)])
+  
+  #Set default forecast value of 0
+  forecast <- 0
+  
+  #Get day of the year (May 5th is either 125 or 126)
+  dayofyear <- yday(tst$Date[i])
+  
+  #Has the seed been sown?
+  sowed = dayofyear >= 125
+  
+  #Reset the harvested boolean when the crop is sown
+  if(dayofyear == 125){
+    harvested = FALSE
+  }
+  
+  #If the crop is sown and not yet harvested
+  if(sowed && !harvested){
+    
+    #If the difference between the previous prediction and the next is more than 500,
+    #then the model has predicted that the crop was harvested, else predict
+    if((as.numeric(predict(ar.fit)$pred) + combo$Trend[testing_index[i]]) < -10){
+      #If crop was harvested, return Maize.AboveGround.Wt forecast of 0
+      harvested = TRUE
+      forecast = -1 * year.tot
+    } else {
+      #If the crop was not harvested, return the AR model online prediction
+      forecast <- as.numeric(predict(ar.fit)$pred) + combo$Trend[testing_index[i]]
+      #Do not return a negative forecast
+      forecast <- max(0, forecast)
+    }
+  }
+  
+  #Update forecasts list
+  ar.forecast[i] <- forecast
+  year.tot <- year.tot + forecast
+}
+
+#Total RMSE
+sqrt(mean((ar.forecast - tst$Maize.Wt.Change)^2))
