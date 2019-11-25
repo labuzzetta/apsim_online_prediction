6a4d04fc326209fe2d51d188115cb11c12fde23b
clabuzze <clabuzze@iastate.edu>
2019-11-24 21:51:01 -0600

    Create training, testing, and secret datasets

--- data-raw/run_simulation.R
+++ data-raw/run_simulation.R
@@ -5,5 +5,16 @@ simulation <- apsimx(file = "special_project_model.apsimx",
               src.dir = "./data-raw/archive", 
               silent = TRUE, value = 'report', cleanup = TRUE)
 
+#Partition datasets (keep secret unused until final evaluation at end of the semester)
+training <- report %>% filter(year(Date) < 2005) %>%
+  mutate(year = year(Date), yday = yday(Date))
+testing <- report %>% filter(year(Date) >= 2005 & year(Date) < 2012) %>%
+  mutate(year = year(Date), yday = yday(Date))
+secret <- report %>% filter(year(Date) >= 2012) %>%
+  mutate(year = year(Date), yday = yday(Date))
+
 #Save RData to the data directory
 usethis::use_data(simulation, overwrite=TRUE)
+usethis::use_data(training, overwrite=TRUE)
+usethis::use_data(testing, overwrite=TRUE)
+usethis::use_data(secret, overwrite=TRUE)
