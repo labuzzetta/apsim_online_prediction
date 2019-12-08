224890f9e84f1382f98bc1d9c283c21462c9af5a
clabuzze <clabuzze@iastate.edu>
2019-12-08 13:55:59 -0600

    Add comments to method checks

--- R/online_emulate_maize.R
+++ R/online_emulate_maize.R
@@ -8,17 +8,21 @@ online_emulate_maize <- function(train, test, pred_var, pred_type, method){
   library(mgcv)
   library(tidyr)
   
+  #List of required variables for online prediction
   required <- c("Weather.Rain", "Weather.Radn", "Weather.MaxT", "Weather.MeanT", 
                 "Weather.MinT", "Weather.VPD", "Date", "Maize.Phenology.CurrentStageName")
   
+  #Check training and testing data.frames for required variables
   if(!all(required %in% colnames(train))) stop("Check required column names in training data")
   if(!all(required %in% colnames(test))) stop("Check required column names in testing data")
   if(!(pred_var %in% colnames(train))) stop("Variable to predict not contained in training data")
   if(!(pred_var %in% colnames(test))) stop("Variable to predict not contained in testing data")
   
-  if(!(method %in% c("online_accumulate", "online_change", "no_update"))) stop("Invalid method type")
+  #Check selected prediction type is valid
+  if(!(pred_type %in% c("online_accumulate", "online_change", "no_update"))) stop("Invalid prediction type (pred_type)")
   
-  if!(method %in% c("rf", "nnet", "ar", "gam", "lm"))
+  #Check method type is supported
+  if(!(method %in% c("rf", "nnet", "ar", "gam", "lm"))) stop("Invalid method type (method)")
   
   
   
