0cb1da31730b63dcd45d455f5de07461e2561ef0
clabuzze <clabuzze@iastate.edu>
2019-12-09 15:13:41 -0600

    Delete unneccesary methods files

--- R/simulation.R
+++
@@ -1,9 +0,0 @@
-#' Simulation
-#' 
-#' 35 Year APSIM Next Generation Simulation to use for Online Prediction
-#' 
-#' @format A data.frame with the following columns:
-#'   
-#'   \code{CheckpointID}: Unique integer id of observation
-#'   
-"simulation"
