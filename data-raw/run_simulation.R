6662b702cc407cd27fbf21d131d6bf7dac771674
clabuzze <clabuzze@iastate.edu>
2019-12-09 15:23:35 -0600

    Update datasets to include "growing"

--- data-raw/run_simulation.R
+++ data-raw/run_simulation.R
@@ -5,13 +5,6 @@ simulation <- apsimx(file = "special_project_model.apsimx",
               src.dir = "./data-raw/archive", 
               silent = TRUE, value = 'report', cleanup = TRUE)
 
-#Partition datasets (keep secret unused until final evaluation at end of the semester)
-training <- simulation %>% filter(year(Date) < 2005) %>%
-  mutate(year = year(Date), yday = yday(Date))
-testing <- simulation %>% filter(year(Date) >= 2005 & year(Date) < 2012) %>%
-  mutate(year = year(Date), yday = yday(Date))
-secret <- simulation %>% filter(year(Date) >= 2012) %>%
-  mutate(year = year(Date), yday = yday(Date))
 simulation <- simulation %>% mutate(year = year(Date), yday = yday(Date))
 
 simulation[which(simulation$Maize.Phenology.CurrentStageName == ""),]$Maize.Phenology.CurrentStageName <- NA;
@@ -23,6 +16,11 @@ simulation <- simulation %>% tidyr::fill("Maize.Phenology.CurrentStageName")
 simulation$growing <- dplyr::if_else(condition = simulation$Maize.Phenology.CurrentStageName != "StartYear" &
                                        simulation$Maize.Phenology.CurrentStageName != "Harvest", 
                                      1, 0)
+
+#Partition datasets (keep secret unused until final evaluation at end of the semester)
+training <- simulation %>% filter(year(Date) < 2005)
+testing <- simulation %>% filter(year(Date) >= 2005 & year(Date) < 2012)
+secret <- simulation %>% filter(year(Date) >= 2012)
 
 #Save RData to the data directory
 usethis::use_data(simulation, overwrite=TRUE)
